cd0cb56f78ccc9700858a0f1d8777b4f1d129faa
Leszek Ślażyński <slazyk@gmail.com>
2014-06-20 21:00:42 +0200

    Initial Commit

---
+++ Observable-Swift/Observable-Swift.h
@@ -0,0 +1,19 @@
+//
+//  Observable-Swift.h
+//  Observable-Swift
+//
+//  Created by Leszek Ślażyński on 20/06/14.
+//  Copyright (c) 2014 Leszek Ślażyński. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Observable-Swift.
+FOUNDATION_EXPORT double Observable_SwiftVersionNumber;
+
+//! Project version string for Observable-Swift.
+FOUNDATION_EXPORT const unsigned char Observable_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Observable_Swift/PublicHeader.h>
+
+

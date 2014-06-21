b96db6be325ec8665a8db008233ce73e31e36982
Leszek Ślażyński <slazyk@gmail.com>
2014-06-21 14:20:13 +0200

    Added more tests showing current semantics and features

--- Observable-SwiftTests/ObservableTests.swift
+++ Observable-SwiftTests/ObservableTests.swift
@@ -12,6 +12,73 @@ import Observable
 
 class ObservableTests: XCTestCase {
     
+    // if handler takes 2 arguments it should be (oldValue, newValue)
+    func testFullHandler() {
+        var x = Observable(0)
+        var t : (Int, Int) = (-1, -1)
+        x.afterChange += { t = ($0, $1) }
+        
+        x <- 1
+        XCTAssertEqual(t.0, 0, "Should receive correct old value")
+        XCTAssertEqual(t.1, 1, "Should receive correct new value")
+        
+        x <- 2
+        XCTAssertEqual(t.0, 1, "Should receive correct old value")
+        XCTAssertEqual(t.1, 2, "Should receive correct new value")
+    }
+    
+    // if handler takes 1 argument it should be newValue
+    func testSimpleHandler() {
+        var x = Observable(0)
+        var t = -1
+        x.afterChange += { t = $0 }
+        
+        x <- 1
+        XCTAssertEqual(t, 1, "Should receive correct new value")
+        
+        x <- 2
+        XCTAssertEqual(t, 2, "Should receive correct new value")
+    }
+    
+    func testConversions() {
+        var x = Observable(22)
+        var y = Observable(20)
+        
+        // 0 is needed here to infer int
+        let sum1 = 0 + x + y
+        XCTAssertEqual(sum1, 42, "Should sum up correctly")
+
+        let sum2 = x as Int + y
+        XCTAssertEqual(sum2, 42, "Should sum up correctly")
+        
+        let sum3 : Int = x + y
+        XCTAssertEqual(sum3, 42, "Should sum up correctly")
+    }
+
+    func testMultipleHandlers() {
+        var x = Observable(0)
+        var a = -1
+        var b = -1
+        x.afterChange += { a = $0 }
+        x.afterChange += { b = $0 }
+        
+        x <- 42
+        XCTAssertEqual(a, 42, "Should fire first handler")
+        XCTAssertEqual(b, 42, "Should fire second handler")
+    }
+    
+    func testBeforeAfter() {
+        var x = Observable(0)
+        var y = -1
+        
+        x.beforeChange += { y = $0 }
+        x.afterChange += { (v: Int) in XCTAssertEqual(y, v, "Should be set in before"); y = 2 * v }
+    
+        x <- 21
+    
+        XCTAssertEqual(y, 42, "Should be set in after")
+    }
+    
     func testShorthand() {
         var x = Observable(0)
         var y = -1
@@ -23,6 +90,20 @@ class ObservableTests: XCTestCase {
         
         x <- 42
         XCTAssertEqual(y, x, "Should add to afterChange")
+    }
+    
+    func testRemove() {
+        var x = Observable(0)
+        var y = 0
+        
+        let observer = x.afterChange += { (_,_) in y += 1 }
+        
+        for i in 0..5 { x <- i }
+        
+        x.afterChange -= observer
+        
+        for i in 0..5 { x <- i }
+        XCTAssertEqual(y, 5, "Should not update after removed")
     }
     
     func testPairObservable() {
@@ -107,20 +188,6 @@ class ObservableTests: XCTestCase {
         XCTAssertEqual(full, "John Snow", "Should update when field changes")
     }
     
-    func testRemove() {
-        var x = Observable(0)
-        var y = 0
-        
-        let observer = x.afterChange += { (_,_) in y += 1 }
-        
-        for i in 0..5 { x <- i }
-        
-        x.afterChange -= observer
-        
-        for i in 0..5 { x <- i }
-        XCTAssertEqual(y, 5, "Should not update after removed")
-    }
-    
     func testMultipleTimes() {
         var x = Observable(0)
         var y = 0
@@ -203,6 +270,29 @@ class ObservableTests: XCTestCase {
         XCTAssertEqual(y, 7, "Should increment for refered object or a copy")
     }
 
+    func testCopySementics() {
+        var x = Observable(0)
+        var y = x;
+        y <- 1
+        XCTAssertEqual(x.value, 0, "Original should stay unchanged")
+        XCTAssertEqual(y.value, 1, "Copy should change")
+    }
+    
+    func testCopyObserversSemantics() {
+        var x = Observable(0)
+        var y = 0
+        x.afterChange += { (_,_) in y += 1 }
+        var z = x
+        
+        z <- 10
+        x <- 20
+        
+        XCTAssertEqual(z.value, 10)
+        XCTAssertEqual(x.value, 20)
+        
+        // well... this is kind of unfortunate, but expected since x was copied
+        XCTAssertEqual(y, 2, "Should be called for both x and z")
+    }
     
     func testStoredReferences() {
         let x = WritableObservableReference(0)

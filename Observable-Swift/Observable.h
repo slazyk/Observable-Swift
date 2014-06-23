11f8a84f264dc1c5369ccac45999134fe70f3be9
Leszek Ślażyński <slazyk@gmail.com>
2014-06-24 01:19:14 +0200

    Fix header: "Observable-Swift" -> "Observable"

--- Observable-Swift/Observable.h
+++ Observable-Swift/Observable.h
@@ -9,11 +9,11 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for Observable-Swift.
-FOUNDATION_EXPORT double Observable_SwiftVersionNumber;
+FOUNDATION_EXPORT double ObservableVersionNumber;
 
 //! Project version string for Observable-Swift.
-FOUNDATION_EXPORT const unsigned char Observable_SwiftVersionString[];
+FOUNDATION_EXPORT const unsigned char ObservableVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Observable_Swift/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <Observable/PublicHeader.h>
 
 
